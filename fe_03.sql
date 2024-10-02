893b959382be775b426c883bbb6935cc6290453c
fizikello <58490580+fizikello@users.noreply.github.com>
2024-10-02 15:19:22 +0200

    update for season 2024/2025

---
+++ fe_03.sql
@@ -0,0 +1,3 @@
+-- details
+select * from "details_t01"
+where "DATE" = '202410011509'
---
+++ fe_05.sql
@@ -0,0 +1,3 @@
+select * from "scrap_performance"
+--WHERE "TECHNOLOGY" != 'test'
+where "DATE" = '202410011509'
